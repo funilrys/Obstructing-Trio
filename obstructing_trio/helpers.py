cab265666ac707a74cf5e06ccffb54f4738f678b
funilrys <contact@funilrys.com>
2017-06-02 10:36:35 +0200

    Add write_file() + add execute_save_cmd() + add read_file() + add convert_JSON_to_dict() + add format_list() + add save_dict_to_JSON()

---
+++ obstructing_trio/helpers.py
@@ -0,0 +1,82 @@
+#!/bin/env python
+
+# Obstructing Trio -  Python module/library for saving the list of contributors of a given public Github project.
+# Copyright (C) 2017  Funilrys - Nissar Chababy <contact at funilrys dot com>
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Original Version: https://github.com/funilrys/Obstructing-Trio
+
+from json import dump, loads
+
+from process import Process
+
+
+def write_file(content, destination):
+    """Write a content in a given file
+
+    :param content: A string, the content we have to write into the file
+    :param destination: A string, A path to a file where we're going to save the content
+    """
+
+    with open(destination, 'w') as file:
+        file.write(content)
+
+
+def execute_save_cmd(self, command, destination):
+    """Execute and save the result of a command into a defined file.
+
+    :param command: A string, the command to execute in shell format
+    :param destination: A string, the location where we're going to save the result(s)
+    """
+
+    result = Process(command).execute()
+
+    write_file(result, destination)
+    return
+
+
+def read_file(file_to_read):
+    """Read the content of a given file
+
+    :param file_to_read: A string, a path to the file we have to read
+    """
+
+    with open(file_to_read, 'r') as file:
+        funilrys = file.read()
+    return funilrys
+
+
+def convert_JSON_to_dict(data):
+    """Convert a JSON into a dictionary
+
+    :param data: A string, a JSON formated string
+    """
+
+    return loads(data)
+
+
+def format_list(data):
+    """Sort and remove duplicate from a given list
+
+    :param data: A list, the list to format
+    """
+
+    return sorted(list(set(data)), key=str.lower)
+
+
+def save_dict_to_JSON(data, destination):
+    """Save a dictionary into a JSON format
+
+    :param data: A dict
+    :param destination: A string, A path to a file where we're going to write the converted dict into a JSON format
+    """
+
+    with open(destination, 'w') as file:
+        dump(data, file, ensure_ascii=False, indent=4, sort_keys=True)
