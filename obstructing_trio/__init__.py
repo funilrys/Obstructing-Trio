6bbc336ceb8180349ca2d1c0bae8b8fdcbc7b155
funilrys <contact@funilrys.com>
2017-07-01 11:26:52 +0200

    Add get()

---
+++ obstructing_trio/__init__.py
@@ -0,0 +1,20 @@
+# Obstructing Trio -  Python module/library for saving the list of contributors of a given public Github repository into a JSON file.
+# Copyright (C) 2017  Funilrys - Nissar Chababy <contact at funilrys dot com>
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Original Version: https://github.com/funilrys/Obstructing-Trio
+
+from .core import Core
+
+
+def get(github_repository):
+    """Send data and perform Core()"""
+
+    return Core(github_repository).get()
